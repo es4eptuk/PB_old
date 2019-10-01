9eedafe1b59b64d1b6f02f6de44669bae69fe15f
David Chagalidze <chagalidze@gmail.com>
2019-10-01 18:04:09 +0500

    исправил заголовок модального окна
    Signed-off-by: David Chagalidze <chagalidze@gmail.com>

--- kanban.php
+++ kanban.php
@@ -787,7 +787,7 @@ foreach ($arr_tickets as &$ticket) {
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Опишите причину переноса карточки</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle">Выберете сотрудника</h5>
                     <!--<button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>-->
