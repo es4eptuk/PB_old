7ec9a11b2dfef6346946338a146c07e2a8095533
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2020-01-28 17:00:26 +0500

    - Перевод класса Log на PDO

--- page/log.php
+++ page/log.php
@@ -5,28 +5,30 @@ class Log
     function __construct()
     {
         global $database_server, $database_user, $database_password, $dbase;
-        $this->link_log = mysql_connect($database_server, $database_user, $database_password) or die('Не удалось соединиться: ' . mysql_error());
-        mysql_set_charset('utf8', $this->link_log);
-        //echo 'Соединение успешно установлено';
-        mysql_select_db($dbase) or die('Не удалось выбрать базу данных');
+        $dsn = "mysql:host=$database_server;dbname=$dbase;charset=utf8";
+        $opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $this->pdo = new PDO($dsn, $database_user, $database_password, $opt);
     }
     function add($class, $message)
     {
         $date    = date("Y-m-d H:i:s");
         $user_id = intval($_COOKIE['id']);
         $query   = "INSERT INTO `system_log` (`id`, `method`, `log`, `update_user`, `update_date`) VALUES (NULL, '$class', '$message', $user_id, '$date')";
-        $result = mysql_query($query) or die($query);
+        $result = $this->pdo->query($query);
         return $result;
     }
     function get_all()
     {
         $query = "SELECT * FROM `system_log`";
-        $result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
-        while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $result = $this->pdo->query($this->query);
+        while ($line = $result->fetch()) {
             $log_array[] = $line;
         }
-        // Освобождаем память от результата
-        mysql_free_result($result);
+
         if (isset($log_array))
             return $log_array;
     }
