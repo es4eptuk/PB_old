90da8cdde1d9120a6d567f914a5d4273d71fa2f7
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2020-01-29 13:44:19 +0500

    - Перевод скрипта поиска позиций на PDO

--- tt.php
+++ tt.php
@@ -1,21 +1,26 @@
-<? 
+<?
+error_reporting(E_ALL);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+
 include 'include/config.inc.php';
 $term = $_GET['term'];
 
 $output = '';
         $str_arr = array();
         global $database_server, $database_user, $database_password, $dbase;
-    	
-    	$link = mysql_connect($database_server, $database_user, $database_password)
-        or die('Не удалось соединиться: ' . mysql_error());
-        mysql_set_charset('utf8',$link);
-        //echo 'Соединение успешно установлено';
-        mysql_select_db($dbase) or die('Не удалось выбрать базу данных');
+        $dsn = "mysql:host=$database_server;dbname=$dbase;charset=utf8";
+        $opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $pdo = new PDO($dsn, $database_user, $database_password, $opt);
+
         $query = "SELECT id,title,vendor_code,assembly FROM pos_items WHERE (title LIKE '%$term%' OR vendor_code LIKE '%$term%')";
-        
-        $result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
-        
-        while( $line = mysql_fetch_array($result, MYSQL_ASSOC)){
+
+        $result = $pdo->query($query);
+        while ($line = $result->fetch()) {
             $pos_array[] = $line; 
             }
          
@@ -23,13 +28,8 @@ $output = '';
             
             foreach ($pos_array as $row) {
                 array_push($str_arr, "\"". $row['id'] ."::" . $row['vendor_code'] ."::" . $row['title'] . "\"");
-                
-                //array_push($str_arr, $row['title']);
-                 //$str_arr[] =  "1111";
             }
-            
-            
-            
+
             $s = "[".implode(",", $str_arr)."]";
             echo $s;
         }
