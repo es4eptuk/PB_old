44c850dd8cd161a5718d611f650ce5900d7a7ee9
ieremeev03 <54796944+ieremeev03@users.noreply.github.com>
2020-02-06 10:59:13 +0500

    - Перевод на PDO result.php - Изменение почты при отправке списаний

--- result.php
+++ result.php
@@ -1,18 +1,29 @@
 <?
 include 'include/class.inc.php';
+
+global $database_server, $database_user, $database_password, $dbase;
+$dsn = "mysql:host=$database_server;dbname=$dbase;charset=utf8";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, $database_user, $database_password, $opt);
+
+
 $id_pos = 125;
 //$pos_assambly = $position->get_pos_in_assembly($id_pos);
 
-$query = "SELECT robots.id FROM `check` INNER JOIN robots ON check.robot = robots.id WHERE check.update_date >= '2019-09-01 00:00:00' AND id_check = 105";
-$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
+$query = "SELECT robots.id FROM `check` INNER JOIN robots ON check.robot = robots.id WHERE check.update_date >= '2019-10-01 00:00:00' AND id_check = 105";
+$result = $pdo->query($query);
 echo "<table border=1>";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+while ($line = $result->fetch()) {
            // $id_assembly = $line['id_assembly'];
             //$kit_array_count[$id_pos][$id_kit0] = $line['count'];
             $id = $line['id'];
-            $query2 = "SELECT robots.version, robots.number, robots.name, tickets.description, tickets.result_description, tickets.date_create FROM tickets INNER JOIN robots ON tickets.robot = robots.id WHERE tickets.robot = $id AND tickets.date_create > '2019-09-05 00:00:00' AND tickets.assign_time != '0000-00-00 00:00:00'";
-            $result2 = mysql_query($query2) or die('Запрос не удался: ' . mysql_error());
-            while ($line2 = mysql_fetch_array($result2, MYSQL_ASSOC)) {
+            $query2 = "SELECT robots.version, robots.number, robots.name, tickets.description, tickets.result_description, tickets.date_create FROM tickets INNER JOIN robots ON tickets.robot = robots.id WHERE tickets.robot = $id AND tickets.date_create > '2020-01-01 00:00:00' AND tickets.assign_time != '0000-00-00 00:00:00'";
+            $result2 = $pdo->query($query2);
+            while ($line2 = $result2->fetch()) {
                 $ticket_array[] = $line2;
                 
                 echo "<tr><td>".$line2['version'].".".$line2['number']."</td><td>".$line2['name']."</td><td>".$line2['description']."</td><td>".$line2['result_description']."</td><td>".$line2['date_create']."</td></tr>";
