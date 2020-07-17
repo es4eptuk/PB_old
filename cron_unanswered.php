0a7d52446066617e91c9f14e866fcbbe771288e1
Baydin Alexander <baidin_kzu@mail.ru>
2020-07-17 19:29:13 +0500

    v1.2.9 -включил лог для унансер

--- cron_unanswered.php
+++ cron_unanswered.php
@@ -2,4 +2,9 @@
 
 include 'include/class.inc.php';
 
+
+$log = print_r(print_r(time()), true);
+file_put_contents( 'log__crone.txt', $log . PHP_EOL, FILE_APPEND);
+
+
 $telegramAPI->getUnanswered();
