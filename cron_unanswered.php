2a889f7bd3ebc35738250bba71ec335d2c65ffe5
Baydin Alexander <baidin_kzu@mail.ru>
2020-07-20 11:45:52 +0500

    v1.2.11 -лог в отправке вкл

--- cron_unanswered.php
+++ cron_unanswered.php
@@ -3,15 +3,14 @@ $path_parts = pathinfo($_SERVER['SCRIPT_FILENAME']); // определяем д�
 chdir($path_parts['dirname']); // задаем директорию выполнение скрипта
 
 //
-$dd = date('Y-m-d H:i:s --- '. $path_parts);
-$log = print_r($dd, true);
+//$dd = date('Y-m-d H:i:s --- '. $path_parts['dirname']);
+$log = print_r($_SERVER, true);
 file_put_contents( 'log__crone.txt', $log . PHP_EOL, FILE_APPEND);
-$i = $_SERVER['REQUEST_URI'];
-
 //
+/*$i = $_SERVER['REQUEST_URI'];
 $i2 = stristr($i, '?', true);
 $log = print_r($i.'/'.$i2, true);
-file_put_contents( 'log__crone.txt', $log . PHP_EOL, FILE_APPEND);
+file_put_contents( 'log__crone.txt', $log . PHP_EOL, FILE_APPEND);*/
 
 include 'include/class.inc.php';
 
